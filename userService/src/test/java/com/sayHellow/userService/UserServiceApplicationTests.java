7e0b507b76f0be27b6112bd0bf3bf1d8a1ec45aa
rodrigo.paris <rparis@k12.com>
2024-04-30 15:39:48 -0300

    first commit

---
+++ userService/src/test/java/com/sayHellow/userService/UserServiceApplicationTests.java
@@ -0,0 +1,30 @@
+package com.sayHellow.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+	@Value("${message:Hello default}")
+	private String message;
+
+	@RequestMapping("/message")
+	String getMessage() {
+		return this.message;
+	}
+}
