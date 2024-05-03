ec537675097703685ef21f18a81c955063ed65ce
LucianoCabanillas98 <lucianocabanillas98@gmail.com>
2024-05-03 17:13:57 -0300

    Refactor Model
    - Refactored model according to our requirements

--- userService/src/main/java/com/sayHellow/user/entity/user/Client.java
+++ userService/src/main/java/com/sayHellow/user/entity/user/Client.java
@@ -1,5 +1,9 @@
 package com.sayHellow.user.entity.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
 public class Client extends User{
     public Client(final Long id, final String name, final String lastname, final String username, final String email, final String address, final double score) {
         super(id, name, lastname, username, email, address, score);
